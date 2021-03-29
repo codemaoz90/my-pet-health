5d621e24701f519d542fe2a09c6593741a919e21
Ivette <ivparejo@gmail.com>
2021-03-29 16:26:02 +0200

    Favicon

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from "react"
 import Landing from "./pages/Landing"
 import Splash from "./pages/Splash"
-
+import { BrowserRouter as Router, Switch,Route} from "react-router-dom";
 
 function App(){
 	return (
-		<div>
-			<Splash/>
-			<Landing />
-		</div>
+		<Router>
+            <Switch>
+                <Route path="/" exact>
+					<Splash/>
+                </Route>
+                <Route path="/landing">                
+					<Landing />
+                </Route>
+            </Switch>  
+        </Router>
+	
+			
+			
+
 	)	
 }
 
--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -1,8 +1,31 @@
 import React from "react"
+import {Container, Col , Row, Image, Button} from "react-bootstrap"
+import "../pages/splash.css"
+import logo from "../assets/icons/logo.png"
+import {Link} from "react-router-dom";
 
 function Splash (){
     return(
-        <p>Soc un splash</p>
+        <Container fluid className="splash d-flex flex-column justify-content-center align-items-center ">
+            <Row className=" justify-content-center">
+                <Col xs={10} md lg={4} className="d-flex justify-content-center ">
+                    <Image className="logo" src={logo} alt="logo" /> 
+                    
+                </Col>
+               
+            </Row>
+            <Row className=" justify-content-center mt-2">
+                <Col xs={10} md lg={4} className="d-flex justify-content-center ">
+                    <p className="splash-text mb-0">MyPetHealth</p>
+                   
+                </Col>
+                <Col xs md lg={12} className="justify-content-center mt-4">
+                    <Link to="/landing">
+                        <Button className="btn-splash w-100">Enter</Button>
+                    </Link>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 export default Splash
